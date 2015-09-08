f8f60c669ba611e4fb7143563e5d23b081b5bb8c
dphaener <dphaener@gmail.com>
2015-09-08 10:20:28 -0700

    Capitalize header columns by default
    This PR adds a default functionality to the table component that
    will make it capitalize all header columns by default.
    
    You can pass in the prop `capitalize=false` to override this
    behavior.

--- src/head.jsx
+++ src/head.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
 export default class Head extends React.Component{
-
   static propTypes = {
-    row: React.PropTypes.object
+    row: React.PropTypes.object,
+    capitalize: React.PropTypes.bool
+  }
+
+  static defaultProps = {
+    capitalize: true
   }
 
   headings() {
     return Object.keys(this.props.row).map((name) => {
-      return <th key={name}>{name}</th>;
+      return <th key={name}>{this.props.capitalize ? this.capitalize(name) : name}</th>;
     });
+  }
+
+  capitalize(str) {
+    return `${str.charAt(0).toUpperCase()}${str.substring(1)}`
   }
 
   render(){
